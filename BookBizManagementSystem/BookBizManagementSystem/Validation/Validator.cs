a7f4ff3f00def7d915eb1bc97704708cf019c677
AbramGirgis <89590024+AbramGirgis@users.noreply.github.com>
2022-10-03 23:39:01 -0400

    Add files via upload

---
+++ BookBizManagementSystem/BookBizManagementSystem/Validation/Validator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+
+namespace BookBizManagementSystem.Validation
+{
+    public static class Validator
+    {
+        //Numerical
+        public static bool NumIsValid(TextBox textBoxInput, int size1, int size2)
+        {
+            return (Regex.IsMatch(textBoxInput.Text, @"^\d{" + size1 + "," + size2 + "}$"));
+        }
+
+        //Alphapetical
+        public static bool AlphapetIsValid(TextBox textBoxInput, int size1, int size2)
+        {
+            return (Regex.IsMatch(textBoxInput.Text, @"^[a-zA-Z]{" + size1 + "," + size2 + "}$"));
+        }
+
+        //Alphapetical and spaces
+        public static bool AlphapeTwzSpaceeIsValid(TextBox textBoxInput, int size1, int size2)
+        {
+            return (Regex.IsMatch(textBoxInput.Text, @"^[a-zA-Z ]{" + size1 + "," + size2 + "}$"));
+        }
+
+        //Alphanumerical
+        public static bool AlphaNumIsValid(TextBox textBoxInput, int size1, int size2)
+        {
+            return (Regex.IsMatch(textBoxInput.Text, @"^[a-zA-Z0-9]{" + size1 + "," + size2 + "}$"));
+        }
+
+        //Alphanumerical and spaces
+        public static bool AlphaNuMwzSpaceIsValid(TextBox textBoxInput, int size1, int size2)
+        {
+            return (Regex.IsMatch(textBoxInput.Text, @"^[a-zA-Z0-9 ]{" + size1 + "," + size2 + "}$"));
+        }
+
+        //Email Check 1
+        public static bool EmailIsValid(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        //Email check2
+        public static bool EmailIsValid2(string email)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            if (match.Success)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
